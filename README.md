34f86ad42cff9d956352238f851f05b80d441377
Dylan Moreland <dylan@dimo.zone>
2022-10-12 20:53:00 -0400

    Add initial schema and table creation, update README with instructions

--- README.md
+++ README.md
@@ -46,3 +46,11 @@ Here `type` is one of `Submitted`, `Mined`, `Confirmed`. For confirmed transacti
 ## Configuration
 
 The [default settings file](settings.sample.yaml) has reasonable defaults for local development. It assumes you are using the [Hardhat node](https://hardhat.org/hardhat-runner/docs/getting-started#connecting-a-wallet-or-dapp-to-hardhat-network) and has `PRIVATE_KEY_MODE` set to true, which should never be done in production.
+
+## Migrations
+
+To add a new migration, run
+```
+goose -dir migrations create MIGRATION_NAME sql
+```
+This will generate the migration with a timestamp prefix. Once you're satisfied with your migration, run `goose -dir migrations fix` to correct the filename.
---
+++ migrations/00002_create_transactions_table.sql
@@ -0,0 +1,24 @@
+-- +goose Up
+-- +goose StatementBegin
+SET search_path TO meta_transaction_processor, public;
+
+CREATE TABLE meta_transaction_requests(
+    id char(27) NOT NULL
+        CONSTRAINT meta_transaction_requests_id_pkey PRIMARY KEY,
+    to bytea NOT NULL
+        CONSTRAINT meta_transaction_requests_to_check CHECK (length(to) = 32),
+    data bytea,
+    hash bytea
+        CONSTRAINT meta_transaction_requests_hash_key UNIQUE,
+        CONSTRAINT meta_transaction_requests_hash_check CHECK (length(hash) = 32),
+    created_at timestamptz NOT NULL DEFAULT current_timestamp,
+    updated_at timestamptz NOT NULL DEFAULT current_timestamp
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SET search_path TO meta_transaction_processor, public;
+
+DROP TABLE meta_transaction_requests;
+-- +goose StatementEnd

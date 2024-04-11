52440013abc1c6e52742637cda2b461129bc2227
Dylan Moreland <79415431+elffjs@users.noreply.github.com>
2024-04-11 13:01:59 -0400

    Test KMS signing with LocalStack (#15)
    * Test KMS signing with LocalStack
    
    * TestContainers setup
    * Clean up some of the arithmetic and byte slice fiddling
    * Clean up names and comments
    
    * Test workflow

--- internal/sender/kms.go
+++ internal/sender/kms.go
@@ -1,6 +1,7 @@
 package sender
 
 import (
+	"bytes"
 	"context"
 	"crypto/x509/pkix"
 	"encoding/asn1"
@@ -13,17 +14,16 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/crypto/secp256k1"
-	"golang.org/x/exp/slices"
 )
 
 var secp256k1N = crypto.S256().Params().N
 var secp256k1HalfN = new(big.Int).Div(secp256k1N, big.NewInt(2))
 
 type kmsSender struct {
-	keyID   string
-	pub     []byte
-	address common.Address
-	client  *kms.Client
+	keyID     string
+	publicKey []byte
+	address   common.Address
+	client    *kms.Client
 }
 
 func (s *kmsSender) Address() common.Address {
@@ -38,75 +38,61 @@ func (s *kmsSender) Sign(ctx context.Context, hash common.Hash) ([]byte, error) 
 		Message:          hash[:],
 	}
 
-	out, err := s.client.Sign(ctx, signInput)
+	signOutput, err := s.client.Sign(ctx, signInput)
 	if err != nil {
 		return nil, err
 	}
 
-	sig := new(ecdsaSigFields)
-	_, err = asn1.Unmarshal(out.Signature, sig)
+	var signOutputSig ecdsaSigValue
+	_, err = asn1.Unmarshal(signOutput.Signature, &signOutputSig)
 	if err != nil {
 		return nil, err
 	}
-
-	sigR, sigS := sig.R.Bytes, sig.S.Bytes
 
 	// Correct S, if necessary, so that it's in the lower half of the group.
-	sigSNum := new(big.Int).SetBytes(sigS)
-	if sigSNum.Cmp(secp256k1HalfN) > 0 {
-		sigS = new(big.Int).Sub(secp256k1N, sigSNum).Bytes()
+	sigS := signOutputSig.S
+	if sigS.Cmp(secp256k1HalfN) > 0 {
+		sigS = new(big.Int).Sub(secp256k1N, sigS)
 	}
 
-	// Determine whether V ought to be 0 or 1.
-	sigRS := append(fixLen(sigR), fixLen(sigS)...)
-	sigRSV := append(sigRS, 0)
+	sigRSV := make([]byte, 65)
+	signOutputSig.R.FillBytes(sigRSV[:32])
+	sigS.FillBytes(sigRSV[32:64])
 
 	recPub, err := crypto.Ecrecover(hash[:], sigRSV)
 	if err != nil {
 		return nil, err
 	}
 
-	if slices.Equal(recPub, s.pub) {
+	if bytes.Equal(recPub, s.publicKey) {
 		return sigRSV, nil
 	}
 
-	sigRSV = append(sigRS, 1)
+	sigRSV[64] = 1
 	recPub, err = crypto.Ecrecover(hash[:], sigRSV)
 	if err != nil {
 		return nil, err
 	}
 
-	if slices.Equal(recPub, s.pub) {
+	if bytes.Equal(recPub, s.publicKey) {
 		return sigRSV, nil
 	}
 
 	return nil, fmt.Errorf("couldn't choose a working V from the returned R and S")
 }
 
-func fixLen(in []byte) []byte {
-	outStart := 0
-	inLen := len(in)
-	inStart := 0
-
-	if inLen > 32 {
-		inStart = inLen - 32
-	} else if inLen < 32 {
-		outStart = 32 - inLen
-	}
-
-	out := make([]byte, common.HashLength)
-	copy(out[outStart:], in[inStart:])
-	return out
-}
-
-type publicKeyFields struct {
+// subjectPublicKeyInfo respresents the PublicKey field on the KMS GetPublicKey response.
+// See https://docs.aws.amazon.com/kms/latest/APIReference/API_GetPublicKey.html
+type subjectPublicKeyInfo struct {
 	Algorithm        pkix.AlgorithmIdentifier
 	SubjectPublicKey asn1.BitString
 }
 
-type ecdsaSigFields struct {
-	R asn1.RawValue
-	S asn1.RawValue
+// ecdsaSigValue represents the Signature field on the KMS Sign response.
+// See https://docs.aws.amazon.com/kms/latest/APIReference/API_Sign.html#API_Sign_ResponseSyntax
+type ecdsaSigValue struct {
+	R *big.Int
+	S *big.Int
 }
 
 func FromKMS(ctx context.Context, client *kms.Client, keyID string) (Sender, error) {
@@ -115,14 +101,14 @@ func FromKMS(ctx context.Context, client *kms.Client, keyID string) (Sender, err
 		return nil, err
 	}
 
-	cert := new(publicKeyFields)
+	var spki subjectPublicKeyInfo
 
-	_, err = asn1.Unmarshal(pubResp.PublicKey, cert)
+	_, err = asn1.Unmarshal(pubResp.PublicKey, &spki)
 	if err != nil {
 		return nil, err
 	}
 
-	pub, err := crypto.UnmarshalPubkey(cert.SubjectPublicKey.Bytes)
+	pub, err := crypto.UnmarshalPubkey(spki.SubjectPublicKey.Bytes)
 	if err != nil {
 		return nil, err
 	}
@@ -131,5 +117,5 @@ func FromKMS(ctx context.Context, client *kms.Client, keyID string) (Sender, err
 
 	addr := crypto.PubkeyToAddress(*pub)
 
-	return &kmsSender{keyID: keyID, pub: pubBytes, address: addr, client: client}, nil
+	return &kmsSender{keyID: keyID, publicKey: pubBytes, address: addr, client: client}, nil
 }
--- internal/sender/sender.go
+++ internal/sender/sender.go
@@ -7,6 +7,8 @@ import (
 )
 
 type Sender interface {
+	// Address returns the Ethereum address for the signer.
 	Address() common.Address
+	// Sign takes in a hash and signs it using the sender's private key.
 	Sign(ctx context.Context, hash common.Hash) ([]byte, error)
 }
